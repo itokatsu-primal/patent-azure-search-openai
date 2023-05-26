e6a81b41c4b922eca0c3dd0accad5fab2fa2adc5
itokatsu-primal <134254405+itokatsu-primal@users.noreply.github.com>
2023-05-26 16:41:17 +0900

    Update ExampleList.tsx

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "リチウム硫黄電池における主な技術課題は何ですか。",
+        value: "リチウム硫黄電池における主な技術課題は何ですか。"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "リチウム硫黄電が優れている点は何ですか。", value: "リチウム硫黄電が優れている点は何ですか。" },
+    { text: "リチウム硫黄電は何の用途で利用されていますか。", value: "リチウム硫黄電は何の用途で利用されていますか。" }
 ];
 
 interface Props {
